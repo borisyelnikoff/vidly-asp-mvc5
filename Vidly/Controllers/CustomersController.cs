838d4a89e0b0d29050b5a8dc30ae8e421705ecfa
Boris Yelnikoff <borys.yelnikof@gmail.com>
2020-07-10 08:45:03 +0200

    Added Customers & Movies controllers and views

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+       // private ModelContext _context = new ModelContext();
+        // GET: Customer
+        protected override void Dispose(bool disposing)
+        {
+            //_context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //return View(customers);
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //return View(customers.Find(c => c.Id == id));
+            return View();
+        }
+    }
+}
