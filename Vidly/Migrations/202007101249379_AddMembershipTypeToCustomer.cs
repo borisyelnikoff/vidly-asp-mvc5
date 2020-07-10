334e4a6cb3ffa9db603b210621dfc17a1230b934
Boris Yelnikoff <borys.yelnikof@gmail.com>
2020-07-10 14:52:34 +0200

    Added MembershipType to Customer and updated DB

---
+++ Vidly/Migrations/202007101249379_AddMembershipTypeToCustomer.cs
@@ -0,0 +1,35 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipTypeToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Short(nullable: false),
+                        Name = c.String(),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Short(nullable: false),
+                        DiscountRate = c.Short(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "MembershipTypeId", c => c.Short(nullable: false));
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropColumn("dbo.Customers", "MembershipTypeId");
+            DropTable("dbo.MembershipTypes");
+        }
+    }
+}
