007e3cbf1acc1399b4aa1a5d130d484268947812
Boris Yelnikoff <borys.yelnikof@gmail.com>
2020-07-10 07:58:47 +0200

    Defined application model

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        //[Required]
+        public int Id { get; set; }
+        //[Required]
+        public string Name { get; set; }
+       /* [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime AddDate { get; set; }
+        [Required]
+        public short NumberInStock { get; set; }*/
+    }
+}
